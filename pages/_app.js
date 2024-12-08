d34fd9dd029b512f42ee3a2e6221ff5380ca79a3
mkrish1825 <131790479+mkrish1825@users.noreply.github.com>
2024-12-08 22:24:39 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import NavBar from "../components/navBar/nav";
-import Layout from "../components/navBar/layout";
+import NavBar from "../components/navBar/nav.module.css";
+import Layout from "../components/navBar/layout.js";
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }) {
